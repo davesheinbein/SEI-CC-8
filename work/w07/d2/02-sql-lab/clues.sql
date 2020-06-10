9144e37e74eb16fe4f17d18676a2b2d6dc613aba
David Sheinbein <davidsheinbein@Davids-MacBook-Pro.local>
2020-06-10 08:51:56 -0700

    update

--- work/w07/d2/02-sql-lab/clues.sql
+++ work/w07/d2/02-sql-lab/clues.sql
@@ -4,6 +4,10 @@
  
 -- Write SQL query here
 
+SELECT code, name, region, population   
+FROM country
+WHERE region = 'Southern Europe' 
+ORDER BY region, population DESC;
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
@@ -12,11 +16,25 @@
 -- Write SQL query here
 
 
+SELECT language
+FROM countrylanguage
+WHERE countrycode = 'VAT';
+
+
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 -- nearby country speaks nothing but that language.
 
 -- Write SQL query here
+
+SELECT countrycode
+FROM countrylanguage
+WHERE language = 'Italian' AND percentage = 100.0;
+
+-- SELECT name
+-- FROM country
+-- WHERE code = 'ITA' OR code = 'SMR' OR code = 'CHE'
+-- AND region = 'Southern Europe';
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
@@ -24,7 +42,13 @@
 -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
 -- be flying to.
 
+
 -- Write SQL query here
+
+SELECT name 
+FROM city 
+WHERE countrycode = 'SMR';
+
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
@@ -33,12 +57,28 @@
 
 -- Write SQL query here
 
+SELECT name 
+FROM city
+WHERE name LIKE 'Serr%';
+
+SELECT countrycode
+FROM city
+WHERE name = 'Serra';
+
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
 -- Write SQL query here
+
+
+-- SELECT * FROM country 
+-- WHERE code = 'BRA';
+
+SELECT name 
+FROM city
+WHERE id = 211;
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
@@ -56,4 +96,8 @@
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+SELECT name
+FROM city
+WHERE population = 91084;
 
